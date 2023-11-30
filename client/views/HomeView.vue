eb9446640b597194f3aee95b991122ddec5610dd
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-11-29 23:15:22 -0500

    Split home view into two halves

--- client/views/HomeView.vue
+++ client/views/HomeView.vue
@@ -8,17 +8,46 @@ const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
 
 <template>
   <main>
-    <h1>Home Page</h1>
-    <section>
-      <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
-      <h1 v-else>Please login!</h1>
-    </section>
-    <PostListComponent />
+    <div class="split-wrapper">
+      <div class="split left">
+        <h1>Home Page</h1>
+        <section>
+          <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
+          <h1 v-else>Please login!</h1>
+        </section>
+        <PostListComponent />
+      </div>
+      <div class="split right"></div>
+    </div>
   </main>
 </template>
 
 <style scoped>
 h1 {
   text-align: center;
+}
+
+.split-wrapper {
+  display: flex;
+  min-height: 100vh;
+}
+
+/* Split the screen in half */
+.split {
+  padding: 1em;
+  float: left;
+  background: var(--darker-bg);
+}
+
+/* Control the left side */
+.left {
+  height: 100%;
+  width: 75%;
+}
+
+/* Control the right side */
+.right {
+  flex-grow: 1;
+  padding: 1em;
 }
 </style>
