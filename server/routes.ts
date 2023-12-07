8a1474819b601c3ef26133f0773225d4860463e5
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-12-07 16:52:07 -0500

    Change assets search by name to be more RESTful

--- server/routes.ts
+++ server/routes.ts
@@ -286,7 +286,7 @@ class Routes {
     return Responses.assets(assets);
   }
 
-  @Router.get("/assetsSearchByName")
+  @Router.get("/assets/search/:name")
   async getAssetsByName(asset_name?: string) {
     let assets;
     if (asset_name) {
