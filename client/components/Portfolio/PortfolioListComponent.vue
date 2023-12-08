84c899c02c14e9a149d8cdb0232c30cae7121e34
Jonatan Fontanez <jonfon01@mit.edu>
2023-12-07 23:59:14 -0500

    Start switching from post to portfolio

--- client/components/Portfolio/PortfolioListComponent.vue
+++ client/components/Portfolio/PortfolioListComponent.vue
@@ -1,36 +1,27 @@
 <script setup lang="ts">
-import CreatePostForm from "@/components/Post/CreatePostForm.vue";
-import EditPostForm from "@/components/Post/EditPostForm.vue";
-import PostComponent from "@/components/Post/PostComponent.vue";
+import CreatePortfolioComponent from "./CreatePortfolioComponent.vue";
+import PortfolioComponent from "./PortfolioComponent.vue";
 import { useUserStore } from "@/stores/user";
 import { fetchy } from "@/utils/fetchy";
 import { storeToRefs } from "pinia";
 import { onBeforeMount, ref } from "vue";
-import SearchPostForm from "./SearchPostForm.vue";
 
 const { isLoggedIn } = storeToRefs(useUserStore());
 
 const props = defineProps(["searchEnabled", "createPostEnabled", "startingFilter"]);
 
 const loaded = ref(false);
-let posts = ref<Array<Record<string, string>>>([]);
-let editing = ref("");
-let searchAuthor = ref("");
+let portfolios = ref<Array<Record<string, string>>>([]);
 
-async function getPosts(author?: string) {
-  let query: Record<string, string> = author !== undefined ? { author } : {};
-  let postResults;
+async function getPosts(owner?: string) {
+  let query: Record<string, string> = owner !== undefined ? { owner } : {};
+  let portfolioResults;
   try {
-    postResults = await fetchy("/api/posts", "GET", { query });
+    portfolioResults = await fetchy("/api/portfolios", "GET", { query });
   } catch (_) {
     return;
   }
-  searchAuthor.value = author ? author : "";
-  posts.value = postResults;
-}
-
-function updateEditing(id: string) {
-  editing.value = id;
+  portfolios.value = portfolioResults;
 }
 
 onBeforeMount(async () => {
@@ -45,18 +36,12 @@ onBeforeMount(async () => {
 
 <template>
   <section v-if="isLoggedIn && props.createPostEnabled">
-    <h2>Create a post:</h2>
-    <CreatePostForm @refreshPosts="getPosts" />
+    <h2>Create a portfolio:</h2>
+    <CreatePortfolioComponent @refreshPosts="getPosts" />
   </section>
-  <div class="row">
-    <h2 v-if="!searchAuthor">Posts:</h2>
-    <h2 v-else>Posts by {{ searchAuthor }}:</h2>
-    <SearchPostForm v-if="props.searchEnabled" @getPostsByAuthor="getPosts" />
-  </div>
-  <section class="posts" v-if="loaded && posts.length !== 0">
-    <article v-for="post in posts" :key="post._id">
-      <PostComponent v-if="editing !== post._id" :post="post" @refreshPosts="getPosts" @editPost="updateEditing" />
-      <EditPostForm v-else :post="post" @refreshPosts="getPosts" @editPost="updateEditing" />
+  <section class="portfolios" v-if="loaded && portfolios.length !== 0">
+    <article v-for="portfolio in portfolios" :key="portfolio._id">
+      <PortfolioComponent />
     </article>
   </section>
   <p v-else-if="loaded">No posts found</p>
