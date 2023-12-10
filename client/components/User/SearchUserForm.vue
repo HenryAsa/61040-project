e8e4f6e5a1f7095f1f463097448e094a55acebd8
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-12-10 15:34:08 -0500

    Change post stuff to user stuff

--- client/components/User/SearchUserForm.vue
+++ client/components/User/SearchUserForm.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const author = ref("");
-const emit = defineEmits(["getPostsByAuthor"]);
+const username = ref("");
+const emit = defineEmits(["getUsersByName"]);
 </script>
 
 <template>
-  <form @submit.prevent="emit('getPostsByAuthor', author)" class="pure-form">
+  <form @submit.prevent="emit('getUsersByName', username)" class="pure-form">
     <fieldset>
-      <legend>Search by Author</legend>
-      <input id="author" type="text" v-model="author" placeholder="Username" />
+      <legend>Search by Username</legend>
+      <input id="author" type="text" v-model="username" placeholder="Username" />
       <button type="submit" class="pure-button pure-button-primary">Search</button>
     </fieldset>
   </form>
