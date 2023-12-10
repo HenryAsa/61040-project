638d3507d3685682b46618a3291da68a36e455d2
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-12-10 14:38:17 -0500

    Make buttons a bit nicer

--- client/components/Portfolio/CopyPortfolioComponent.vue
+++ client/components/Portfolio/CopyPortfolioComponent.vue
@@ -38,10 +38,10 @@ onBeforeMount(async () => {
       <button v-if="!copying" class="btn-small pure-button pure-button-primary" @click="toggleCopy">Copy</button>
       <form v-else @submit.prevent="copyPortfolio(content)">
         <textarea id="content" v-model="content" placeholder="Portfolio name" required> </textarea>
-        <menu>
-          <li><button class="btn-small pure-button-primary pure-button" type="submit">Save</button></li>
-          <li><button class="btn-small pure-button" @click="toggleCopy">Cancel</button></li>
-        </menu>
+        <div class="buttons">
+          <button class="btn-small pure-button-primary pure-button" type="submit">Save</button>
+          <button class="btn-small pure-button" @click="toggleCopy">Cancel</button>
+        </div>
       </form>
     </div>
     <div v-else>
@@ -62,5 +62,11 @@ h1 {
 
 p {
   margin: auto;
+}
+
+.base {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
